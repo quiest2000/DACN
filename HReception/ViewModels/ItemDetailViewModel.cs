15437a3f9d7aeef90a5733e8e71edfdbd7f4df6a
GMOHCM-Quinn <quinn@runsystem.net>
2018-05-25 08:40:41 +0700

    first initialize empty solution

---
+++ HReception/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HReception.Models;
+
+namespace HReception.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
